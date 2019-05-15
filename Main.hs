f59b20dd6af92a0319baf51a15a53ab5b9e8ce8d
Daniel Trstenjak <daniel.trstenjak@gmail.com>
2019-05-15 20:34:28 +0200

    Updates for hsimport 0.10.0

--- Main.hs
+++ Main.hs
@@ -3,20 +3,18 @@ module Main where
 import qualified Language.Haskell.Exts as HS
 import HsImport
 
-type HsImportDecl = HS.ImportDecl HS.SrcSpanInfo
-
 main :: IO ()
 main = hsimport $ defaultConfig { prettyPrint = prettyPrint, findImportPos = findImportPos }
    where
       -- This is a bogus implementation of prettyPrint, because it doesn't handle the
       -- qualified import case nor does it considers any explicitely imported or hidden symbols.
-      prettyPrint :: HsImportDecl -> String
+      prettyPrint :: ImportDecl -> String
       prettyPrint (HS.ImportDecl { HS.importModule = HS.ModuleName _ modName }) =
          "import " ++ modName
 
       -- This findImportPos implementation will always add the new import declaration
       -- at the end of the current ones. The data type ImportPos has the two constructors
       -- After and Before.
-      findImportPos :: HsImportDecl -> [HsImportDecl] -> Maybe ImportPos
+      findImportPos :: ImportDecl -> [ImportDecl] -> Maybe ImportPos
       findImportPos _         []             = Nothing
       findImportPos newImport currentImports = Just . After . last $ currentImports
